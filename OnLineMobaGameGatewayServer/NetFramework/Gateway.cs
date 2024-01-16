1d13cf3ff451af766fdecfac0d3619be44cab4d4
hankangwen <13538898378@163.com>
2024-01-16 23:32:23 +0800

    [简易分布式框架]网关服务器添加UDP通讯

--- OnLineMobaGameGatewayServer/NetFramework/Gateway.cs
+++ OnLineMobaGameGatewayServer/NetFramework/Gateway.cs
@@ -51,6 +51,16 @@ public static class Gateway
     private static float _pingInterval = 2;
 
     /// <summary>
+    /// 接收客户端的udp
+    /// </summary>
+    private static UdpClient _receiveClientUdp;
+
+    /// <summary>
+    /// 接收服务端的udp
+    /// </summary>
+    private static UdpClient _receiveServerUdp;
+
+    /// <summary>
     /// 连接服务器
     /// </summary>
     /// <param name="ip">ip地址</param>
@@ -62,6 +72,8 @@ public static class Gateway
         IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, port);
         listenfd.Bind(iPEndPoint);
         listenfd.Listen(0);
+
+        _receiveClientUdp = new UdpClient((IPEndPoint)listenfd.LocalEndPoint);
 
         Console.WriteLine("服务器启动成功");
         while (true)
@@ -124,6 +136,11 @@ public static class Gateway
         serverState.socket = socket;
 
         serverStates.Add(socket, serverState);
+
+        //udp
+        _receiveServerUdp = new UdpClient((IPEndPoint)gateway.LocalEndPoint);
+        _receiveServerUdp.BeginReceive(ReceiveUdpServerCallback, serverState);
+
         //接收消息
         ByteArray byteArray = serverState.readBuffer;
         socket.BeginReceive(byteArray.bytes, byteArray.writeIndex, byteArray.Remain, SocketFlags.None, ReveiceServerCallback, serverState);
@@ -247,6 +264,8 @@ public static class Gateway
             //创建描述客户端的对象
             ClientState state = new ClientState();
             state.socket = socket;
+
+            _receiveClientUdp.BeginReceive(ReceiveUdpClientCallback, state);
 
             uint guid = MyGuid.GetGuid();
             state.guid = guid;
@@ -460,249 +479,52 @@ public static class Gateway
         clientStates.Remove(state.socket);
     }
 
+    #region Udp
 
-    ///// <summary>
-    ///// 服务端socket
-    ///// </summary>
-    //public static Socket listenfd;
+    private static void ReceiveUdpClientCallback(IAsyncResult ar)
+    {
+        ClientState state = (ClientState)ar.AsyncState;
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] receiveBuf = _receiveClientUdp.EndReceive(ar, ref iPEndPoint);
+        ServerType serverType = (ServerType)receiveBuf[0];
 
-    ///// <summary>
-    ///// 客户端字典
-    ///// </summary>
-    //public static Dictionary<Socket, ClientState> states = new Dictionary<Socket, ClientState>();
+        //要给发送消息的服务器的IP地址
+        IPEndPoint serverIPEndPoint = (IPEndPoint)type2ss[serverType].socket.RemoteEndPoint;
+        byte[] sendBytes = new byte[receiveBuf.Length + 3];
+        //打包guid
+        sendBytes[0] = (byte)(state.guid >> 24);
+        sendBytes[1] = (byte)((state.guid >> 16) & 0xff);
+        sendBytes[2] = (byte)((state.guid >> 8) & 0xff);
+        sendBytes[3] = (byte)(state.guid & 0xff);
 
-    ///// <summary>
-    ///// 用于检测的列表
-    ///// </summary>
-    //public static List<Socket> sockets = new List<Socket>();
+        Array.Copy(receiveBuf, 1, sendBytes, 4, receiveBuf.Length - 1);
 
-    //private static float _pingInterval = 2;
+        _receiveServerUdp.Send(sendBytes, sendBytes.Length, serverIPEndPoint);
 
-    ///// <summary>
-    ///// 连接服务器
-    ///// </summary>
-    ///// <param name="ip">ip地址</param>
-    ///// <param name="port">端口号</param>
-    //public static void Connect(string ip, int port)
-    //{
-    //    listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-    //    IPAddress iPAddress = IPAddress.Parse(ip);
-    //    IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, port);
-    //    listenfd.Bind(iPEndPoint);
-    //    listenfd.Listen(0);
+        _receiveClientUdp.BeginReceive(ReceiveUdpClientCallback, state);
+    }
 
-    //    Console.WriteLine("服务器启动成功");
-    //    while (true)
-    //    {
-    //        sockets.Clear();
-    //        //放服务端的Socket
-    //        sockets.Add(listenfd);
-    //        //放客户端的Socket
-    //        foreach (Socket socket in states.Keys)
-    //        {
-    //            sockets.Add(socket);
-    //        }
-    //        Socket.Select(sockets, null, null, 1000);
-    //        for (int i = 0; i < sockets.Count; i++)
-    //        {
-    //            Socket s = sockets[i];
-    //            if (s == listenfd)
-    //            {
-    //                //有客户端要连接
-    //                Accept(listenfd);
-    //            }
-    //            else
-    //            {
-    //                //客户端发消息过来了
-    //                Receive(s);
-    //            }
-    //        }
-    //        CheckPing();
-    //    }
-    //}
 
-    ///// <summary>
-    ///// 接收客户端连接
-    ///// </summary>
-    ///// <param name="listenfd">服务端的Socket</param>
-    //private static void Accept(Socket listenfd)
-    //{
-    //    try
-    //    {
-    //        Socket socket = listenfd.Accept();
-    //        Console.WriteLine("Accept " + socket.RemoteEndPoint.ToString());
-    //        //创建描述客户端的对象
-    //        ClientState state = new ClientState();
-    //        state.socket = socket;
+    private static void ReceiveUdpServerCallback(IAsyncResult ar)
+    {
+        ServerState state = (ServerState)ar.AsyncState;
 
-    //        state.lastPingTime = GetTimeStamp();
-    //        states.Add(socket, state);
-    //    }
-    //    catch (SocketException e)
-    //    {
-    //        Console.WriteLine("Accept 失败" + e.Message);
-    //    }
-    //}
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        byte[] receiveBuf = _receiveServerUdp.EndReceive(ar, ref iPEndPoint);
 
-    ///// <summary>
-    ///// 接收客户端发送过来的消息
-    ///// </summary>
-    ///// <param name="socket">客户端的Socket</param>
-    //private static void Receive(Socket socket)
-    //{
-    //    ClientState state = states[socket];
-    //    ByteArray readBuffer = state.readBuffer;
+        uint guid = (uint)(receiveBuf[0] << 24 |
+                    receiveBuf[1] << 16 |
+                    receiveBuf[2] << 8 |
+                    receiveBuf[3]);
 
-    //    if (readBuffer.Remain <= 0)
-    //    {
-    //        readBuffer.MoveBytes();
-    //    }
-    //    if (readBuffer.Remain <= 0)
-    //    {
-    //        Console.WriteLine("Receive 失败， 数组不够大");
-    //        return;
-    //    }
+        IPEndPoint clientIpEndPoint = (IPEndPoint)id2cs[guid].socket.RemoteEndPoint;
 
-    //    int count = 0;
-    //    try
-    //    {
-    //        count = socket.Receive(readBuffer.bytes, readBuffer.writeIndex, readBuffer.Remain, SocketFlags.None);
-    //    }
-    //    catch (SocketException e)
-    //    {
-    //        Console.WriteLine("Receive 失败，" + e.Message);
-    //        return;
-    //    }
-    //    //客户端主动关闭
-    //    if (count <= 0)
-    //    {
-    //        Console.WriteLine("Socket Close:" + socket.RemoteEndPoint.ToString());
-    //        return;
-    //    }
-    //    readBuffer.writeIndex += count;
-    //    //处理消息
-    //    OnReveiceData(state);
-    //    readBuffer.MoveBytes();
-    //}
+        Array.Copy(receiveBuf, 4, receiveBuf, 0, receiveBuf.Length - 4);
 
-    ///// <summary>
-    ///// 处理消息
-    ///// </summary>
-    ///// <param name="state">客户端对象</param>
-    //private static void OnReveiceData(ClientState state)
-    //{
-    //    ByteArray readBuffer = state.readBuffer;
-    //    byte[] bytes = readBuffer.bytes;
-    //    int readIndex = readBuffer.readIndex;
+        _receiveClientUdp.Send(receiveBuf, receiveBuf.Length, clientIpEndPoint);
 
-    //    if (readBuffer.Length <= 2)
-    //        return;
+        _receiveServerUdp.BeginReceive(ReceiveUdpServerCallback, state);
+    }
 
-    //    //解析总长度
-    //    short length = (short)(bytes[readIndex + 1] * 256 + bytes[readIndex]);
-    //    //收到的消息没有解析出来的多
-    //    if (readBuffer.Length < length)
-    //        return;
-
-    //    readBuffer.readIndex += 2;
-
-    //    int nameCount = 0;
-    //    string protoName = MsgBase.DecodeName(readBuffer.bytes, readBuffer.readIndex, out nameCount);
-    //    if (protoName == "")
-    //    {
-    //        Console.WriteLine("OnReveiceData 失败，协议名为空");
-    //        return;
-    //    }
-    //    readBuffer.readIndex += nameCount;
-
-    //    int bodyLength = length - nameCount;
-    //    MsgBase msgBase = MsgBase.Decode(protoName, readBuffer.bytes, readBuffer.readIndex, bodyLength);
-    //    readBuffer.readIndex += bodyLength;
-    //    readBuffer.MoveBytes();
-
-    //    //通过反射调用客户端发过来的协议对应的方法
-    //    MethodInfo mi = typeof(MsgHandler).GetMethod(protoName);
-    //    if (mi != null)
-    //    {
-    //        //要执行方法的参数
-    //        object[] o = { state, msgBase };
-    //        mi.Invoke(null, o);
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine("OnReceiveData 反射失败");
-    //    }
-
-    //    if(readBuffer.Length > 2)
-    //    {
-    //        OnReveiceData(state);
-    //    }
-    //}
-
-    ///// <summary>
-    ///// 发送消息
-    ///// </summary>
-    ///// <param name="state">客户端对象</param>
-    ///// <param name="msgBase">消息</param>
-    //public static void Send(ClientState state, MsgBase msgBase)
-    //{
-    //    if (state == null || !state.socket.Connected)
-    //        return;
-
-    //    //编码
-    //    byte[] nameBytes = MsgBase.EncodeName(msgBase);
-    //    byte[] bodyBytes = MsgBase.Encode(msgBase);
-    //    int len = nameBytes.Length + bodyBytes.Length;
-    //    byte[] sendBytes = new byte[len + 2];
-    //    sendBytes[0] = (byte)(len % 256);
-    //    sendBytes[1] = (byte)(len / 256);
-    //    Array.Copy(nameBytes, 0, sendBytes, 2, nameBytes.Length);
-    //    Array.Copy(bodyBytes, 0, sendBytes, 2 + nameBytes.Length, bodyBytes.Length);
-
-    //    try
-    //    {
-    //        state.socket.Send(sendBytes, 0, sendBytes.Length, 0);
-    //    }
-    //    catch (SocketException e)
-    //    {
-    //        Console.WriteLine("Send 失败" + e.Message);
-    //    }
-    //}
-
-    ///// <summary>
-    ///// 获取时间戳
-    ///// </summary>
-    ///// <returns></returns>
-    //public static long GetTimeStamp()
-    //{
-    //    TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-    //    return Convert.ToInt64(ts.TotalSeconds);
-    //}
-
-    ///// <summary>
-    ///// 心跳检测
-    ///// </summary>
-    //private static void CheckPing()
-    //{
-    //    foreach (ClientState state in states.Values)
-    //    {
-    //        if(GetTimeStamp() - state.lastPingTime > _pingInterval * 4)
-    //        {
-    //            Console.WriteLine("心跳机制，断开连接：", state.socket.RemoteEndPoint);
-    //            //关闭客户端
-    //            Close(state);
-    //            return;
-    //        }
-    //    }
-    //}
-
-    ///// <summary>
-    ///// 关闭对应客户端
-    ///// </summary>
-    ///// <param name="state">客户端</param>
-    //private static void Close(ClientState state)
-    //{
-    //    state.socket.Close();
-    //    states.Remove(state.socket);
-    //}
+    #endregion
 }
